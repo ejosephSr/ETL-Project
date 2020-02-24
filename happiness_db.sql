1ca146aa2bd1bbe3f58a702f8522df33c6af89f4
Emile Joseph <emmden17@yahoo.com>
2020-02-23 19:34:14 -0600

    initial commits

---
+++ happiness_db.sql
@@ -0,0 +1,9 @@
+ALTER TABLE happiness_2017
+RENAME column happiness_rank to happiness_rank_2017;
+
+SELECT * From happiness_2017
+
+ALTER TABLE happiness_2017
+RENAME column economy_gpd_per_capita_2017 to economy_GDP_per_capita_2017;
+
+SELECT * From happiness_2016
